3bea3ef0da5a17b6c6d212d08b3eb0487c9f1d8d
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-07-06 23:52:58 +0300

    sidebar

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -10,15 +10,29 @@ const Wrap = styled.div`
 `;
 
 const Sidebar = styled.div`
-  width: 200px;
+  z-index: 1;
+  width: 260px;
   flex: none;
-  background: #cccccc;
-  padding: 10px;
+  background: #2e3340;
+  background: -moz-linear-gradient(top, #2e3340 0%, #323441 100%);
+  background: -webkit-linear-gradient(top, #2e3340 0%,#323441 100%);
+  background: linear-gradient(to bottom, #2e3340 0%,#323441 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2e3340', endColorstr='#323441',GradientType=0 );
+  box-shadow: 0 0 10px rgba(0,0,0,0.4);
+`;
+
+const MenuItem = styled.div`
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 20px 30px;
+  text-transform: uppercase;
 `;
 
 const Content = styled.div`
-  padding: 10px;
+  padding: 40px;
   width: 100%;
+  background: #F6F6F6;
 `;
 
 class AppContainer extends PureComponent {
@@ -35,7 +49,10 @@ class AppContainer extends PureComponent {
   render() {
     return (
       <Wrap>
-        <Sidebar>sidebar here</Sidebar>
+        <Sidebar>
+          <MenuItem>Логин</MenuItem>
+          <MenuItem>Пользователи</MenuItem>
+        </Sidebar>
         <Content>{this.props.children}</Content>
       </Wrap>
     );
